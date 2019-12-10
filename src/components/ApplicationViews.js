e82a3c3ef7280ba3862bbb0de27ab2263ada3ee7
Michelle Johnson <bob1234girl@gmail.com>
2019-12-09 20:10:53 -0600

    working on pick kid

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,29 +2,48 @@ import { Route, Redirect } from "react-router-dom";
 import React, { Component } from "react";
 import Register from "./auth/Registration";
 import Login from "./auth/Login"
+import KidList from "./child/ChildList"
 
 export default class ApplicationViews extends Component {
     render() {
-      return (
-        <React.Fragment>
-             <Route path="/login" render={props => {
-            if (this.props.user) {
-              return <Redirect to="/" />
-            } else {
-              return <Login setUser={this.props.setUser} {...props}{...this.props} />
-            }
-          }}
-        />
-         <Route
-          path="/register" render={props => {
-            if (this.props.user) {
-              return <Redirect to="/" />
-            } else {
-              return <Register setUser={this.props.setUser} {...props}{...this.props} />
-            }
-          }}
-        />
-        </React.Fragment>
-    );
-  }
+        return (
+            <React.Fragment>
+                <Route path="/login" render={props => {
+                    if (this.props.user) {
+                        return <Redirect to="/" />
+                    } else {
+                        return <Login setUser={this.props.setUser} {...props}{...this.props} />
+                    }
+                }}
+                />
+                <Route
+                    path="/register" render={props => {
+                        if (this.props.user) {
+                            return <Redirect to="/" />
+                        } else {
+                            return <Register setUser={this.props.setUser} {...props}{...this.props} />
+                        }
+                    }}
+                />
+
+                <Route
+                    exact
+                    path="/kids"
+                    render={props => {
+                        if (this.props.user) {
+                            return <KidList {...props} />;
+                        } else {
+                            return <Redirect to="/kid1" />
+                        }
+                        // Remove null and return the component which will show news articles
+                    }}
+                />
+
+
+                {/* <Route path="/kids/new" render={(props) => {
+                    return <KidForm {...props} />
+                }} /> */}
+            </React.Fragment>
+        );
+    }
 }
