7ad6445b67c30218cf6aa31dd5529417cfb8df96
Michelle Johnson <bob1234girl@gmail.com>
2019-12-16 15:40:02 -0600

    added conditional for parents

--- src/components/rewards/RewardList.js
+++ src/components/rewards/RewardList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 //import the components we will need
 import RewardCard from './RewardCard';
 import RewardManager from '../../modules/RewardManager';
@@ -10,73 +10,77 @@ class RewardList extends Component {
         rewards: [],
     }
 
-componentDidMount(){
-    // console.log("REWARD LIST: ComponentDidMount");
-    // console.log("getAll",RewardManager.getAll())
-    //getAll from RewardManager and hang on to that data; put it in state
-    RewardManager.getAll(sessionStorage.getItem("kidCredentials"))
-    .then((rewards) => {
-        this.setState({
-            rewards: rewards
-        })
-    })
-}
+    componentDidMount() {
+        // console.log("REWARD LIST: ComponentDidMount");
+        // console.log("getAll",RewardManager.getAll())
+        //getAll from RewardManager and hang on to that data; put it in state
+        RewardManager.getAll(sessionStorage.getItem("kidCredentials"))
+            .then((rewards) => {
+                this.setState({
+                    rewards: rewards
+                })
+            })
+    }
 
-deleteReward = id => {
-    RewardManager.delete(id)
-    .then(() => {
-      RewardManager.getAll(sessionStorage.getItem("kidCredentials"))
-      .then((newRewards) => {
-          console.log("new",newRewards)
-        this.setState({
-            rewards: newRewards
-        })
-      })
-    })
-  }
-  pickRandomReward = () => {
-      let randomValue = this.state.rewards[Math.floor(Math.random() * this.state.rewards.length)];
-      alert(`YOU GOT: ${randomValue.name}`)
-  }
+    deleteReward = id => {
+        RewardManager.delete(id)
+            .then(() => {
+                RewardManager.getAll(sessionStorage.getItem("kidCredentials"))
+                    .then((newRewards) => {
+                        console.log("new", newRewards)
+                        this.setState({
+                            rewards: newRewards
+                        })
+                    })
+            })
+    }
+    pickRandomReward = () => {
+        let randomValue = this.state.rewards[Math.floor(Math.random() * this.state.rewards.length)];
+        alert(`YOU GOT: ${randomValue.name}`)
+    }
+    isParent = () => {
+        let Parent = JSON.parse(sessionStorage.getItem("credentials"))
+        console.log("RewardList: Render", Parent.isParent);
 
-render(){
-    console.log("RewardList: Render");
-    return(
-        <>
-        
-            <br/>
-            <section className="section-content">
-            <Button type="button"
-                className="btn"
-                variant="dark" ariant="outline-secondary"
-                onClick={() => {this.props.history.push("/rewards/new")}}>
-                Add Reward
+    }
+
+    render() {
+
+        return (
+            <>
+                <br />
+                <section className="section-content">
+                    <Button type="button"
+                        className="btn"
+                        variant="dark" ariant="outline-secondary"
+                        onClick={() => { this.props.history.push("/rewards/new") }}>
+                        Add Reward
             </Button>
-            </section>
-            <br/>  
-            <div className="container-cards">
-            {this.state.rewards.map(reward =>
-            <RewardCard 
-            key={reward.id} 
-            reward={reward} 
-            deleteReward={this.deleteReward}
-            {...this.props}/>
-            )}
-            </div>
-            <br/>
-            <section className="section-content">
-            <Button type="button"
-                className="btn"
-                variant="dark" ariant="outline-secondary"
-                onClick={() => {this.pickRandomReward()}}
-                >
-                Pick Random Reward
+                </section>
+                <br />
+                <div className="container-cards">
+                    {this.state.rewards.map(reward =>
+                        <RewardCard
+                            key={reward.id}
+                            reward={reward}
+                            deleteReward={this.deleteReward}
+                            {...this.props} />
+                    )}
+                </div>
+                <br />
+                <section className="section-content">
+                    <Button type="button"
+                        className="btn"
+                        variant="dark" ariant="outline-secondary"
+                        onClick={() => { this.pickRandomReward() }}
+                    >
+                        Pick Random Reward
             </Button>
-            </section>
-            <br/>
-        </>
-    )
-  }
+                </section>
+                <br />
+            </>
+        )
+    }
 }
 
 export default RewardList
