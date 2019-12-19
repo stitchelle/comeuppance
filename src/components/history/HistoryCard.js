740f20744599e1c62e89ca6ff768e889c0b08679
Michelle Johnson <bob1234girl@gmail.com>
2019-12-19 15:48:56 -0600

    (-) sign shows in from of punishments on the history page

--- src/components/history/HistoryCard.js
+++ src/components/history/HistoryCard.js
@@ -7,15 +7,24 @@ class HistoryCard extends Component {
         return (
             <Card>
                 <Card.Body>
-                    <Card.Subtitle>
-                        hi{this.props.history.point.numberOfPoints}
-                    </Card.Subtitle>
+                    
+        {(this.props.history.comeuppance.comeuppanceType === 1) ?
+            <>
+                <Card.Subtitle>{this.props.history.point.numberOfPoints}</Card.Subtitle>
+                </> :
+       
+            
+       <Card.Subtitle> - {this.props.history.point.numberOfPoints}</Card.Subtitle> 
+            
+        }
+        
+                    
                     <Card.Title>
                         {this.props.history.comeuppance.name}
                     </Card.Title>
                     <Card.Text>
-                        {/* {new Date(this.props.history.timestamp).getFullYear()}-
-                          {new Date(this.props.history.timestamp).getMonth()}-{new Date(this.props.history.timestamp).getDate()} */}
+                        {new Date(this.props.history.timestamp).getFullYear()}-
+                          {new Date(this.props.history.timestamp).getMonth()}-{new Date(this.props.history.timestamp).getDate()}
                     </Card.Text>
                 </Card.Body>
             </Card>
