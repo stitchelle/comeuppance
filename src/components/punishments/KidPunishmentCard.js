3c4fa01a2472fb21644eacf8fd28bce4520eeb53
Michelle Johnson <bob1234girl@gmail.com>
2019-12-19 14:39:15 -0600

    picked punishment now posts into history

--- src/components/punishments/KidPunishmentCard.js
+++ src/components/punishments/KidPunishmentCard.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
 import { Button, Card, CardGroup } from 'react-bootstrap'
+import HistoryManager from "../../modules/HistoryManager"
+
+let picked = {}
+
 
 class KidPunishmentCard extends Component {
+    // for posting into history
+    state = {
+        userId: "",
+        pointId: "",
+        comeuppanceId: "",
+        completed: "",
+        timestamp: "",
+        loadingStatus: false,
+    };
+
+    handleFieldChange = evt => {
+        const stateToChange = {};
+        stateToChange[evt.target.id] = evt.target.value;
+        this.setState(stateToChange);
+    };
+
+    /*  Local method for validation, set loadingStatus, create animal      object, invoke the HistoryManager post method, and redirect to the full KidRewardCard 
+    */
+    constructNewHistory = evt => {
+        let credentials =
+            JSON.parse(sessionStorage.getItem("credentials"))
+        console.log("picked", credentials.id)
+        // evt.preventDefault();
+            this.setState({ loadingStatus: true });
+            const history = {
+                userId: credentials.id,
+                pointId: picked.pointId,
+                comeuppanceId: picked.id,
+                completed: null,
+                timestamp: new Date().toISOString()
+
+            };
+            // Create the history and redirect user to ristory list
+            HistoryManager.post(history)
+            // .then(() => this.props.history.push("/history"));
+        }
+    
     pickRandomPunishment = () => {
         let randomValue = this.props.punishments[Math.floor(Math.random() * this.props.punishments.length)];
         alert(`YOU GOT: ${randomValue.name}`)
+        picked = randomValue
+
+        this.constructNewHistory()
     }
 
     render() {
