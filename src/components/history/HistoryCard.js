697467108ae8924de186c3f154d6bc10ce55027e
Michelle Johnson <bob1234girl@gmail.com>
2019-12-19 16:35:08 -0600

    added checkbox to history

--- src/components/history/HistoryCard.js
+++ src/components/history/HistoryCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card } from "react-bootstrap"
+import { Card, InputGroup, Col, Row } from "react-bootstrap"
 
 class HistoryCard extends Component {
     render() {
@@ -7,25 +7,34 @@ class HistoryCard extends Component {
         return (
             <Card>
                 <Card.Body>
-                    
-        {(this.props.history.comeuppance.comeuppanceType === 1) ?
-            <>
-                <Card.Subtitle>{this.props.history.point.numberOfPoints}</Card.Subtitle>
-                </> :
-       
-            
-       <Card.Subtitle> - {this.props.history.point.numberOfPoints}</Card.Subtitle> 
-            
-        }
-        
-                    
-                    <Card.Title>
-                        {this.props.history.comeuppance.name}
-                    </Card.Title>
-                    <Card.Text>
-                        {new Date(this.props.history.timestamp).getFullYear()}-
+                    <Row>
+                        {(this.props.history.comeuppance.comeuppanceType === 1) ?
+                            <>
+                                <Col>
+                                    <Card.Text>{this.props.history.point.numberOfPoints} Points</Card.Text>
+                                </Col>
+                            </> :
+                            <Col>
+                                <Card.Subtitle> - {this.props.history.point.numberOfPoints} Points</Card.Subtitle>
+                            </Col>
+                        }
+                        <Col>
+                            <Card.Title>
+                                {this.props.history.comeuppance.name}
+                            </Card.Title>
+                        </Col>
+                        <Col>
+                            <Card.Text>
+                                Date: {new Date(this.props.history.timestamp).getFullYear()}-
                           {new Date(this.props.history.timestamp).getMonth()}-{new Date(this.props.history.timestamp).getDate()}
-                    </Card.Text>
+                            </Card.Text>
+                        </Col>
+                        <Col>
+                            <InputGroup.Prepend>
+                                <InputGroup.Checkbox aria-label="checkbox" />
+                            </InputGroup.Prepend>
+                        </Col>
+                    </Row>
                 </Card.Body>
             </Card>
         );
