03782cfec2735764359b3474eca6fc4153f662bb
Michelle Johnson <bob1234girl@gmail.com>
2020-01-06 10:52:53 -0600

    added Kuma image to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 import { Card } from 'react-bootstrap'
 import Helper from './Helper'
+import Logo from './nav/kuma2.png'
+import "../components/Home.css"
 
 class Home extends Component {
   render() {
     if (sessionStorage.getItem("kidCredentials") !== null || Helper.isParent(sessionStorage) !== true) {
       return (
-        <Card.Header>
-          <center><h1>COMEUPPANCE</h1></center>
-        </Card.Header>
+        <>
+          <Card.Header>
+            <center>
+              <h1>COMEUPPANCE</h1>
+            </center>
+          </Card.Header>
+          <Card.Body>
+            <div className="HomeLogoContainer">
+              <img className="KumaLogo" src={Logo}></img>
+            </div>
+          </Card.Body>
+        </>
       )
     } else {
       return (
@@ -17,7 +28,12 @@ class Home extends Component {
             <center><h1>COMEUPPANCE</h1></center>
           </Card.Header>
           <Card.Body>
-            <center><h5>Please Pick Child in Navigation Dropdown</h5></center>
+            <center>
+              <h5>Please Pick Child in Navigation Dropdown</h5>
+              <div className="HomeLogoContainer">
+                <img className="KumaLogo" src={Logo}></img>
+              </div>
+            </center>
           </Card.Body>
         </>
       )
